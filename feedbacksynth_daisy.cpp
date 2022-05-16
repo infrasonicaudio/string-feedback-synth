b199041ecf08ed7490e732c48f91e0864684f670
Nick Donaldson <ndonald2@gmail.com>
2022-05-16 12:08:48 -0600

    Setup intellisense a little better

---
+++ feedbacksynth_daisy.cpp
@@ -0,0 +1,14 @@
+#include <daisy_seed.h> 
+#include <daisysp.h>
+
+using namespace daisy;
+using namespace daisysp;
+
+void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
+{
+}
+
+int main(void)
+{
+    return 0;
+}

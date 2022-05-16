55a455d129f04a50ad12e79c86aa34e9096df4d1
Nick Donaldson <ndonald2@gmail.com>
2022-05-16 12:59:56 -0600

    Basic audio output from macos version

--- feedbacksynth_macos.cpp
+++ feedbacksynth_macos.cpp
@@ -11,21 +11,24 @@
 #include <iostream>
 #include <cstdlib>
 
+static const unsigned int kSampleRate = 48000;
+static const unsigned int kNumChannels = 2;
+
+static daisysp::Oscillator osc;
+
 // Two-channel sawtooth wave generator.
-int saw( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
-         double streamTime, RtAudioStreamStatus status, void *userData )
+int audio_callback( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
+                    double streamTime, RtAudioStreamStatus status, void *userData )
 {
   unsigned int i, j;
-  double *buffer = (double *) outputBuffer;
-  double *lastValues = (double *) userData;
+  float *buffer = (float *) outputBuffer;
   if ( status )
     std::cout << "Stream underflow detected!" << std::endl;
   // Write interleaved audio data.
   for ( i=0; i<nBufferFrames; i++ ) {
-    for ( j=0; j<2; j++ ) {
-      *buffer++ = lastValues[j];
-      lastValues[j] += 0.005 * (j+1+(j*0.1));
-      if ( lastValues[j] >= 1.0 ) lastValues[j] -= 2.0;
+    float samp = osc.Process();
+    for ( j=0; j<kNumChannels; j++ ) {
+      *buffer++ = samp;
     }
   }
   return 0;
@@ -38,16 +41,22 @@ int main()
     std::cout << "\nNo audio devices found!\n";
     exit( 0 );
   }
+
+  osc.Init(static_cast<float>(kSampleRate));
+  osc.SetAmp(0.5);
+  osc.SetFreq(440.0);
+  osc.SetWaveform(daisysp::Oscillator::WAVE_POLYBLEP_TRI);
+
   RtAudio::StreamParameters parameters;
   parameters.deviceId = dac.getDefaultOutputDevice();
-  parameters.nChannels = 2;
+  parameters.nChannels = kNumChannels;
   parameters.firstChannel = 0;
-  unsigned int sampleRate = 44100;
-  unsigned int bufferFrames = 256; // 256 sample frames
-  double data[2] = {0, 0};
+  std::cout << "Outputting to " << dac.getDeviceInfo(parameters.deviceId).name << "\n";
+
+  unsigned int bufferFrames = 128;
   try {
-    dac.openStream( &parameters, NULL, RTAUDIO_FLOAT64,
-                    sampleRate, &bufferFrames, &saw, (void *)&data );
+    dac.openStream( &parameters, NULL, RTAUDIO_FLOAT32,
+                    kSampleRate, &bufferFrames, &audio_callback, nullptr);
     dac.startStream();
   }
   catch ( RtAudioError& e ) {

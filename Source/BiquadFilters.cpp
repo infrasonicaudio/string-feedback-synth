5e317ee22df2b0124f1981a35f925920e620369e
Nick Donaldson <ndonald2@gmail.com>
2022-05-19 10:37:08 -0600

    Templated cascaded filter definitions

--- Source/BiquadFilters.cpp
+++ Source/BiquadFilters.cpp
@@ -1,6 +1,5 @@
 #include "BiquadFilters.h"
 #include <cmath>
-#include <daisysp.h>
 
 #ifdef __arm__
     #include <arm_math.h>
@@ -11,83 +10,50 @@
 
 using namespace infrasonic;
 
-void Biquad2Pole::Init(float sample_rate)
+const BiquadSection::Coefficients BiquadSection::CalculateCoefficients(const FilterType type, 
+                                                                       const float sample_rate,
+                                                                       const float cutoff_hz,
+                                                                       const float q)
 {
-    sample_rate_ = sample_rate;
-    Ts_ = 1.0f / sample_rate;
-    cutoff_hz_ = sample_rate * 0.49f;
-    q_ = 0.70710678118f; // sqrt(2)/2 = "flat"
-    updateCoefficients();
-}
-
-void Biquad2Pole::SetFilterType(const FilterType type)
-{
-    filter_type_ = type;
-    updateCoefficients();
-}
-
-void Biquad2Pole::SetCutoff(const float cutoff_hz)
-{
-    cutoff_hz_ = daisysp::fclamp(cutoff_hz, 1.f, sample_rate_ * 0.5f);
-    updateCoefficients();
-}
-
-void Biquad2Pole::SetQ(const float q)
-{
-    q_ = daisysp::fmax(q, 0.1f);
-    updateCoefficients();
-}
-
-void Biquad2Pole::SetParams(const FilterType type, const float cutoff_hz, const float q)
-{
-    filter_type_ = type;
-    cutoff_hz_ = daisysp::fclamp(cutoff_hz, 1.f, sample_rate_ * 0.5f);
-    q_ = daisysp::fmax(q, 0.1f);
-    updateCoefficients();
-}
-
-void Biquad2Pole::updateCoefficients()
-{
+   Coefficients coefs; 
     float norm;
-    const float K = BQ_TANF(PI_F * cutoff_hz_ * Ts_);
+    const float K = BQ_TANF(PI_F * cutoff_hz * (1.0f / sample_rate));
     const float Ksq = K * K;
 
-    switch (filter_type_) {
+    float &b0 = coefs[0];
+    float &b1 = coefs[1];
+    float &b2 = coefs[2];
+    float &a1 = coefs[3];
+    float &a2 = coefs[4];
+
+    switch (type) {
         case FilterType::HighPass:
-            norm = 1.0f / (1.0f + (K / q_) + Ksq);
-            b0_ = norm;
-            b1_ = -2.0f * b0_;
-            b2_ = b0_;
-            a1_ = 2.0f * (Ksq - 1.0f) * norm;
-            a2_ = (1.0f - (K / q_) + Ksq) * norm; 
+            norm = 1.0f / (1.0f + (K / q) + Ksq);
+            b0 = norm;
+            b1 = -2.0f * b0;
+            b2 = b0;
+            a1 = 2.0f * (Ksq - 1.0f) * norm;
+            a2 = (1.0f - (K / q) + Ksq) * norm; 
             break;
 
         case FilterType::BandPass:
-            norm = 1.0f / (1.0f + (K / q_) + Ksq);
-            b0_ = (K / q_) * norm;
-            b1_ = 0.0f;
-            b2_ = -b0_;
-            a1_ = 2.0f * (Ksq - 1) * norm;
-            a2_ = (1.0f - (K / q_) + Ksq) * norm;
+            norm = 1.0f / (1.0f + (K / q) + Ksq);
+            b0 = (K / q) * norm;
+            b1 = 0.0f;
+            b2 = -b0;
+            a1 = 2.0f * (Ksq - 1) * norm;
+            a2 = (1.0f - (K / q) + Ksq) * norm;
             break;
 
         default: // lowpass
-            norm = 1.0f / (1.0f + (K / q_) + Ksq);
-            b0_ = Ksq * norm;
-            b1_ = 2.0f * b0_;
-            b2_ = b0_;
-            a1_ = 2.0f * (Ksq - 1.0f) * norm;
-            a2_ = (1.0f - (K / q_) + Ksq) * norm; 
+            norm = 1.0f / (1.0f + (K / q) + Ksq);
+            b0 = Ksq * norm;
+            b1 = 2.0f * b0;
+            b2 = b0;
+            a1 = 2.0f * (Ksq - 1.0f) * norm;
+            a2 = (1.0f - (K / q) + Ksq) * norm;
             break;
     }
 
-    // std::cout << std::setprecision(5) 
-    //     << "\nCoefs for f = " << cutoff_hz_ << " and q = " << q_ << "\n"
-    //     << "  b0 = " << b0_ << "\n"
-    //     << "  b1 = " << b1_ << "\n"
-    //     << "  b2 = " << b2_ << "\n"
-    //     << "  a1 = " << a1_ << "\n"
-    //     << "  a2 = " << a2_ << "\n"
-    //     << "\n";
+   return coefs;
 }
-
--- Source/BiquadFilters.h
+++ Source/BiquadFilters.h
@@ -3,16 +3,16 @@
 #define INFS_BIQUADFILTERS_H
 
 #include <cassert>
+#include <array>
+#include <daisysp.h>
 
 namespace infrasonic {
 
 /**
- *  Single precision 2nd order Butterworth biquad filter
- *  with support for mono or stereo sample-by-sample-processing.
- *
- *  TODO: Support for higher order / cascading
+ *  Single precision 2nd order Butterworth biquad filter section with support for stereo processing.
+ *  NOTE: There are templated filter definitions at the bottom of this file for convenience. 
  */
-class Biquad2Pole {
+class BiquadSection {
 
     public:
 
@@ -22,63 +22,138 @@ class Biquad2Pole {
             BandPass
         };
 
-        Biquad2Pole() {}
-        ~Biquad2Pole() {}
+        // Assumes normalized coefficients (a0 == 1)
+        // Ordering numerator then denominator: {b0, b1, b2, a1, a2}
+        using Coefficients = std::array<float, 5>;
 
-        void Init(const float sample_rate);
+        static const Coefficients CalculateCoefficients(const FilterType type, 
+                                                        const float sample_rate,
+                                                        const float cutoff_hz,
+                                                        const float q);
 
-        // Individual param update methods recalculate coefficients
-        // every time one of them is called. To update everything at once,
-        // use SetParams()
+        BiquadSection() {}
+        ~BiquadSection() {}
 
-        void SetFilterType(const FilterType type);
-        void SetCutoff(const float cutoff_hz);
-        void SetQ(const float q);
+        void SetCoefficients(const Coefficients coefficients) { coefs_ = coefficients; }
 
-        /// Update all params simultaneously, recalculating coefficients only once 
-        void SetParams(const FilterType type, const float cutoff_hz, const float q);
-
-        inline float Process(const float in)
-        {
-            return process(in, 0);
-        }
-
-        /// In-place stereo processing
-        inline void ProcessStereo(float &sampL, float &sampR)
-        {
-            sampL = process(sampL, 0);
-            sampR = process(sampR, 1);
-        }
-
-    private:
-
-        float sample_rate_, Ts_;
-
-        FilterType filter_type_ = FilterType::LowPass;
-        float cutoff_hz_, q_;
-
-        // coefs
-        float b0_, b1_, b2_, a1_, a2_;
-
-        // state
-        float s1_[2] = {0, 0};
-        float s2_[2] = {0, 0};
-
-        void updateCoefficients();
-
-        inline float process(const float in, const int channel)
+        inline float Process(const float in, const int channel)
         {
             assert(channel < 2);
 
             // TODO: arm accelerated version
 
+            const float &b0 = coefs_[0];
+            const float &b1 = coefs_[1];
+            const float &b2 = coefs_[2];
+            const float &a1 = coefs_[3];
+            const float &a2 = coefs_[4];
+
             // Transposed direct form 2
-            float y = b0_ * in + s1_[channel];
-            s1_[channel] = s2_[channel] + in * b1_ - a1_ * y;
-            s2_[channel] = b2_ * in - a2_ * y;
+            float y = b0 * in + s1_[channel];
+            s1_[channel] = s2_[channel] + in * b1 - a1 * y;
+            s2_[channel] = b2 * in - a2 * y;
             return y;
         }
+
+    private:
+
+        // coef
+        Coefficients coefs_{0, 0, 0, 0, 0};
+
+        // state
+        float s1_[2] = {0, 0};
+        float s2_[2] = {0, 0};
+
 };
+
+// Templated cascaded butterworth biquad filter
+template<size_t NumSections, BiquadSection::FilterType FilterType>
+class BiquadCascade {
+
+    public:
+        BiquadCascade() {}
+        ~BiquadCascade() {}
+
+        // Individual param update methods recalculate coefficients
+        // every time one of them is called. To update everything at once,
+        // use SetParams()
+
+        void Init(const float sample_rate) {
+            sample_rate_ = sample_rate;
+            cutoff_hz_ = sample_rate * 0.25f;
+            q_ = 0.70710678118f; // sqrt(2)/2 = "flat" for 2nd order
+            updateCoefficients();
+        }
+
+        inline void SetCutoff(const float cutoff_hz)
+        {
+            cutoff_hz_ = daisysp::fclamp(cutoff_hz, 1.f, sample_rate_ * 0.5f);
+            updateCoefficients();
+        }
+
+        inline void SetQ(const float q)
+        {
+            q_ = daisysp::fmax(q, 0.1f);
+            updateCoefficients();
+        }
+
+        /// Update params simultaneously, recalculating coefficients only once 
+        inline void SetParams(const float cutoff_hz, const float q)
+        {
+            cutoff_hz_ = daisysp::fclamp(cutoff_hz, 1.f, sample_rate_ * 0.5f);
+            q_ = daisysp::fmax(q, 0.1f);
+            updateCoefficients();
+        }
+
+        inline float Process(const float in)
+        {
+            float out = in;
+            for (auto &biquad : biquads_) {
+                out = biquad.Process(in, 0);
+            }
+            return out;
+        }
+
+        /// In-place stereo processing
+        inline void ProcessStereo(float &sampL, float &sampR)
+        {
+            for (auto &biquad : biquads_) {
+                sampL = biquad.Process(sampL, 0);
+                sampR = biquad.Process(sampR, 1);
+            }
+        }
+
+    private:
+
+        float sample_rate_;
+        float cutoff_hz_, q_;
+
+        std::array<BiquadSection, NumSections> biquads_;
+
+        inline void updateCoefficients() {
+            for (auto &biquad : biquads_) {
+                biquad.SetCoefficients(BiquadSection::CalculateCoefficients(FilterType, sample_rate_, cutoff_hz_, q_));
+            }
+        }
+};
+
+/// 12dB/Oct resonant lowpass filter (butterworth topology)
+using ButterworthLP12 = BiquadCascade<1, BiquadSection::FilterType::LowPass>;
+
+/// 24dB/Oct resonant lowpass filter (butterworth topology)
+using ButterworthLP24 = BiquadCascade<2, BiquadSection::FilterType::LowPass>;
+
+/// 12dB/Oct resonant highpass filter (butterworth topology)
+using ButterworthHP12 = BiquadCascade<1, BiquadSection::FilterType::HighPass>;
+
+/// 24dB/Oct resonant highpass filter (butterworth topology)
+using ButterworthHP24 = BiquadCascade<2, BiquadSection::FilterType::HighPass>;
+
+/// 12dB/Oct resonant bandpass filter (butterworth topology)
+using ButterworthBP12 = BiquadCascade<1, BiquadSection::FilterType::BandPass>;
+
+/// 24dB/Oct resonant bandpass filter (butterworth topology)
+using ButterworthBP24 = BiquadCascade<2, BiquadSection::FilterType::BandPass>;
 
 }
 
